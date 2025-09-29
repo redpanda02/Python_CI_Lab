0428f4933b6ff389c966ac90eb25c797e8c3f30e
Maurice Varore <ndene154@gmail.com>
2025-09-29 01:15:23 +0000

    Recent changes

--- my_app/calculator.py
+++ my_app/calculator.py
@@ -2,6 +2,7 @@ def add(a: int, b: int) -> int:
     """Return the sum of two numbers."""
     return a + b
 
+
 def subtract(a: int, b: int) -> int:
     """Return the difference of two numbers."""
     return a - b
